8427e775325094ce4e9a22bc22be925d73b92ddf
Bee <duy.ta@sotatek.com>
2025-04-28 21:39:45 +0700

    CU-8698tg1fd implement login page

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
