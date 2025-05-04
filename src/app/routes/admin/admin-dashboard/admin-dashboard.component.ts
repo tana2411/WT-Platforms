db785bca5a503595ac2291234131c17f7c27a84e
Bee <duy.ta@sotatek.com>
2025-05-04 17:53:34 +0700

    CU-8698udkfa fix bug role admin

--- src/app/routes/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/routes/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { CommonLayoutComponent } from 'app/layout/common-layout/common-layout.component';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.scss'],
-  imports: [],
+  imports: [CommonLayoutComponent],
 })
 export class AdminDashboardComponent {}
--- src/app/routes/haulier/haulier-dashboard/haulier-dashboard.component.ts
+++ src/app/routes/haulier/haulier-dashboard/haulier-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonLayoutComponent } from 'app/layout/common-layout/common-layout.component';
 
 @Component({
   selector: 'app-haulier-dashboard',
   templateUrl: './haulier-dashboard.component.html',
   styleUrls: ['./haulier-dashboard.component.scss'],
-  imports: [],
+  imports: [CommonLayoutComponent],
 })
 export class HaulierDashboardComponent {}
