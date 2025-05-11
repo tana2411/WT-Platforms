a1723a10f77020fb7fcf9f210c118a130e50c067
Bee <duy.ta@sotatek.com>
2025-05-11 22:47:20 +0700

    CU-8698tfzg0 implement bidding form and update some UI

--- src/app/routes/market-place/market-place.component.ts
+++ src/app/routes/market-place/market-place.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { CommonLayoutComponent } from 'app/layout/common-layout/common-layout.component';
+import { BiddingFormComponent } from 'app/share/ui/product-detail/bidding-form/bidding-form.component';
 import { FilterComponent } from '../../share/ui/listing/filter/filter.component';
-import { ProductGridComponent } from '../../share/ui/listing/product-grid/product-grid.component';
-import { PaginationComponent } from '../../share/ui/listing/pagination/pagination.component';
 import { ListingFooterComponent } from '../../share/ui/listing/listing-footer/listing-footer.component';
+import { PaginationComponent } from '../../share/ui/listing/pagination/pagination.component';
+import { ProductGridComponent } from '../../share/ui/listing/product-grid/product-grid.component';
 
 @Component({
   selector: 'app-market-place',
@@ -15,10 +17,12 @@ import { ListingFooterComponent } from '../../share/ui/listing/listing-footer/li
     ProductGridComponent,
     PaginationComponent,
     ListingFooterComponent,
+    MatDialogModule,
+    BiddingFormComponent,
   ],
 })
 export class MarketPlaceComponent {
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
   onPageChange(page: number) {
     console.log(page);
@@ -26,5 +30,21 @@ export class MarketPlaceComponent {
 
   onFilterChange(filterParams: any) {
     console.log(filterParams);
+  }
+
+  onBid() {
+    const dialogRef = this.dialog.open(BiddingFormComponent, {
+      maxWidth: '960px',
+      panelClass: 'px-3',
+    });
+
+    // dialogRef.afterClosed().subscribe((result) => {
+    //   if (result === 'buyer-seller') {
+    //     // Handle buyer/seller registration
+    //     this.router.navigateByUrl('/create-account');
+    //   } else if (result === 'haulier') {
+    //     this.router.navigateByUrl('/create-haulier-account');
+    //   }
+    // });
   }
 }
