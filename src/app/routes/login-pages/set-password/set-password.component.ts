42e9decc855236a5cb0ce51d14e7c462c158f495
Bee <duy.ta@sotatek.com>
2025-05-19 14:36:45 +0700

    CU-86990z1mv_Defect- -- 6.2.1.4- Set- password- -- 6.2.1.4- Set- password- -- Scenario:- Entering- and- Confirming- the- New- Password

--- src/app/routes/login-pages/set-password/set-password.component.ts
+++ src/app/routes/login-pages/set-password/set-password.component.ts
@@ -1,26 +1,17 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-  AbstractControl,
-  ValidationErrors,
-} from '@angular/forms';
-import { AuthService } from '../../../services/auth.service';
-import { RouterLink, Router, ActivatedRoute } from '@angular/router';
-import { RequestSetPasswordParams } from 'app/types/requests/auth';
-import { ROUTES_WITH_SLASH } from 'app/constants/route.const';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { TitleCasePipe } from '@angular/common';
-import { checkPasswordStrength, pwdStrengthValidator } from '@app/validators';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { debounceTime } from 'rxjs';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { InputWithConfirmControlComponent } from '@app/ui';
-
-const PASSWORD_PATTERN = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
+import { checkPasswordStrength, pwdStrengthValidator } from '@app/validators';
+import { ROUTES_WITH_SLASH } from 'app/constants/route.const';
+import { RequestSetPasswordParams } from 'app/types/requests/auth';
+import { debounceTime } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-set-password',
@@ -39,10 +30,7 @@ const PASSWORD_PATTERN = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
 export class SetPasswordComponent implements OnInit {
   token: string | null = null;
   formGroup = new FormGroup({
-    password: new FormControl<string | null>(null, [
-      Validators.required,
-      pwdStrengthValidator,
-    ]),
+    password: new FormControl<string | null>(null, [Validators.required, pwdStrengthValidator]),
   });
 
   serverError = signal('');
@@ -55,14 +43,12 @@ export class SetPasswordComponent implements OnInit {
     private route: ActivatedRoute,
     private snackbar: MatSnackBar,
   ) {
-    this.formGroup?.valueChanges
-      .pipe(takeUntilDestroyed(), debounceTime(300))
-      .subscribe((value) => {
-        const { password } = value; // expiryDate: moment type
-        if (password) {
-          this.pwdStrength.set(checkPasswordStrength(password));
-        }
-      });
+    this.formGroup?.valueChanges.pipe(takeUntilDestroyed(), debounceTime(300)).subscribe((value) => {
+      const { password } = value; // expiryDate: moment type
+      if (password) {
+        this.pwdStrength.set(checkPasswordStrength(password));
+      }
+    });
   }
 
   ngOnInit(): void {
@@ -76,59 +62,7 @@ export class SetPasswordComponent implements OnInit {
     });
   }
 
-  // private passwordPatternValidator(
-  //   control: AbstractControl,
-  // ): ValidationErrors | null {
-  //   if (!control.value) {
-  //     return null;
-  //   }
-
-  //   return PASSWORD_PATTERN.test(control.value)
-  //     ? null
-  //     : { passwordStrength: true };
-  // }
-
-  // private passwordMatchValidator(
-  //   control: AbstractControl,
-  // ): ValidationErrors | null {
-  //   const password = control.get('password');
-  //   const confirmPassword = control.get('confirmPassword');
-
-  //   if (!password || !confirmPassword) return null;
-
-  //   if (password.value !== confirmPassword.value) {
-  //     confirmPassword.setErrors({ passwordMismatch: true });
-  //     return { passwordMismatch: true };
-  //   } else {
-  //     confirmPassword.setErrors(null);
-  //     return null;
-  //   }
-  // }
-
-  getErrorMessage(controlName: string): string {
-    const control = this.formGroup.get(controlName);
-    if (!control) return '';
-
-    if (control.hasError('required')) {
-      return 'This is a required field.';
-    }
-
-    if (control.hasError('passwordStrength')) {
-      return 'Passwords must be at least 6 characters and contain at least one uppercase letter, one lowercase letter, and one number.';
-    }
-
-    if (
-      this.formGroup.hasError('passwordMismatch') &&
-      controlName === 'confirmPassword'
-    ) {
-      return 'Passwords do not match.';
-    }
-
-    return '';
-  }
-
   send() {
-    console.log(this.formGroup);
     if (this.loading()) {
       return;
     }
@@ -154,9 +88,7 @@ export class SetPasswordComponent implements OnInit {
         this.router.navigateByUrl(ROUTES_WITH_SLASH.login);
       },
       error: (error) => {
-        if (
-          error?.error?.error?.message === 'Error verifying token : jwt expired'
-        ) {
+        if (error?.error?.error?.message === 'Error verifying token : jwt expired') {
           this.snackbar.open('Token expired.');
         } else {
           this.snackbar.open('Something went wrong.');
