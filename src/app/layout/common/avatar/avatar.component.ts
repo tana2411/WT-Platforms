c8570ffb17be62be207990addf73c8def3fb97cd
Bee <duy.ta@sotatek.com>
2025-05-19 15:16:31 +0700

    CU-8698tg0et implement confirm logout

--- src/app/layout/common/avatar/avatar.component.ts
+++ src/app/layout/common/avatar/avatar.component.ts
@@ -1,16 +1,18 @@
-import { Component, computed, Signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { AuthService } from 'app/services/auth.service';
-import { MatMenuModule } from '@angular/material/menu';
-import { map, tap } from 'rxjs';
-import { RouterModule } from '@angular/router';
-import { IconComponent } from '../icon/icon.component';
+import { Component, inject, Signal } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { ROUTES, ROUTES_WITH_SLASH } from 'app/constants/route.const';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router, RouterModule } from '@angular/router';
+import { ROUTES_WITH_SLASH } from 'app/constants/route.const';
+import { AuthService } from 'app/services/auth.service';
+import { ConfirmModalComponent, ConfirmModalProps } from 'app/share/ui/confirm-modal/confirm-modal.component';
+import { map } from 'rxjs';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-avatar',
-  imports: [MatMenuModule, RouterModule, IconComponent, MatIconModule],
+  imports: [MatMenuModule, RouterModule, IconComponent, MatIconModule, MatDialogModule],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss',
 })
@@ -18,29 +20,42 @@ export class AvatarComponent {
   compactUserName: Signal<string>;
   userName: Signal<string>;
 
-  routes = ROUTES_WITH_SLASH
+  routes = ROUTES_WITH_SLASH;
+  dialog = inject(MatDialog);
+  router = inject(Router);
 
   constructor(private authService: AuthService) {
     this.compactUserName = toSignal(
-      this.authService.user$.pipe(
-        map((user) =>
-          user ? `${user.user.firstName[0]} ${user.user.lastName[0]}` : '',
-        ),
-      ),
+      this.authService.user$.pipe(map((user) => (user ? `${user.user.firstName[0]} ${user.user.lastName[0]}` : ''))),
       {
         initialValue: '',
       },
     );
 
     this.userName = toSignal(
-      this.authService.user$.pipe(
-        map((user) =>
-          user ? `${user.user.firstName} ${user.user.lastName}` : '',
-        ),
-      ),
+      this.authService.user$.pipe(map((user) => (user ? `${user.user.firstName} ${user.user.lastName}` : ''))),
       {
         initialValue: '',
       },
     );
+  }
+
+  onLogout() {
+    this.dialog
+      .open<ConfirmModalComponent, ConfirmModalProps>(ConfirmModalComponent, {
+        maxWidth: '500px',
+        width: '100%',
+        panelClass: 'px-3',
+        data: {
+          title: 'Are you sure you want to log out?',
+        },
+      })
+      .afterClosed()
+      .pipe(takeUntilDestroyed())
+      .subscribe((shouldLogout) => {
+        if (shouldLogout) {
+          this.router.navigateByUrl(ROUTES_WITH_SLASH.logout);
+        }
+      });
   }
 }
---
+++ src/app/share/ui/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export type ConfirmModalProps = {
+  title: string;
+};
+
+@Component({
+  selector: 'app-confirm-modal',
+  imports: [MatButtonModule],
+  templateUrl: './confirm-modal.component.html',
+  styleUrl: './confirm-modal.component.scss',
+})
+export class ConfirmModalComponent {
+  @Input() title = 'Are you sure ?';
+
+  readonly dialogRef = inject(MatDialogRef<ConfirmModalComponent>);
+  readonly props = inject<ConfirmModalProps>(MAT_DIALOG_DATA);
+
+  onOk() {
+    this.dialogRef.close(true);
+  }
+
+  onCancel() {
+    this.dialogRef.close(false);
+  }
+}
