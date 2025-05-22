00ecbacb666109aa55a4f7d9c43df1b2e6b78a0e
Tri Nguyen <tringuyen@b13technology.com>
2025-05-22 11:11:27 +0700

    fetch and display user profile

--- src/app/routes/account-settings/my-profile/my-profile.component.ts
+++ src/app/routes/account-settings/my-profile/my-profile.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { Component, computed, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { User } from 'app/models';
+import { AuthService } from 'app/services/auth.service';
+import { SpinnerComponent } from 'app/share/ui/spinner/spinner.component';
 
 @Component({
   selector: 'app-my-profile',
-  imports: [],
   templateUrl: './my-profile.component.html',
-  styleUrl: './my-profile.component.scss'
+  styleUrl: './my-profile.component.scss',
+  imports: [MatButtonModule, MatIconModule, SpinnerComponent, TitleCasePipe],
 })
 export class MyProfileComponent {
+  authService = inject(AuthService);
+  snackBar = inject(MatSnackBar);
 
+  user: Signal<User | undefined | null>;
+  loading = computed(() => !this.user());
+
+  constructor() {
+    this.user = toSignal(this.authService.user$);
+
+    if (!this.user) {
+      this.snackBar.open('Failed to load profile details. Please try again later.', 'OK', {
+        duration: 3000,
+      });
+    }
+  }
 }
