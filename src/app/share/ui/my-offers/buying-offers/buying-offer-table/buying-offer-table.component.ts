7f28b7d6fa36c212b600364dc4ea92be1e700aba
duyta <duyta@b13technology.com>
2025-06-19 16:36:59 +0700

    CU-8699a01cj-fix-bug-offer-made

--- src/app/share/ui/my-offers/buying-offers/buying-offer-table/buying-offer-table.component.ts
+++ src/app/share/ui/my-offers/buying-offers/buying-offer-table/buying-offer-table.component.ts
@@ -1,13 +1,15 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { FallbackImageDirective } from '@app/directives';
 import { TableBuyingOfferItem } from 'app/models/offer';
+import { getStatusColor } from 'app/share/utils/offer';
 import { PaginationComponent } from '../../../listing/pagination/pagination.component';
 
 @Component({
   selector: 'app-buying-offer-table',
-  imports: [PaginationComponent, MatButtonModule, RouterModule, FallbackImageDirective],
+  imports: [PaginationComponent, MatButtonModule, RouterModule, FallbackImageDirective, TitleCasePipe],
   templateUrl: './buying-offer-table.component.html',
   styleUrl: './buying-offer-table.component.scss',
 })
@@ -16,6 +18,8 @@ export class BuyingOfferTableComponent {
   @Input() page: number = 1;
   @Input() items: TableBuyingOfferItem[] = [];
   @Output() pageChange = new EventEmitter<number>();
+
+  getStatusColor = getStatusColor;
 
   onPageChange(page: number) {
     this.pageChange.emit(page);
