aa33f64a470f69f11a56617a544f203bd4c3c850
duyta <duyta@b13technology.com>
2025-06-24 01:49:16 +0700

    CU-86999w2qv fix bug sale listing

--- src/app/share/ui/listing/product-status/product-status.component.ts
+++ src/app/share/ui/listing/product-status/product-status.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, effect, input, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { ListingMaterial } from 'app/models';
+import { ListingMaterial, ListingType } from 'app/models';
 import { ProductStatus } from 'app/models/product.model';
 import moment from 'moment';
 
@@ -10,20 +10,50 @@ import moment from 'moment';
   templateUrl: './product-status.component.html',
   styleUrl: './product-status.component.scss',
 })
-export class ProductStatusComponent implements OnInit {
-  @Input({ required: true }) product: ListingMaterial | undefined;
-  status: ProductStatus = ProductStatus.Required;
+export class ProductStatusComponent {
+  product = input<ListingMaterial | undefined>(undefined);
+  status = signal<ProductStatus | undefined>(undefined);
   ProductStatus = ProductStatus;
+  fromDate = signal<string | undefined>(undefined);
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      const product = this.product();
+      if (!product) {
+        return;
+      }
 
-  ngOnInit() {}
+      const listingType = product.listingType;
+      const now = new Date();
+      const startDate = product ? product!.startDate : undefined;
+      const endDate = product ? product!.endDate : undefined;
+      const isFutureProduct = startDate ? moment(startDate).isAfter(now) : undefined;
+      const isExpired = endDate ? moment(endDate).isBefore(now) : undefined;
+      const isSold = product.remainingQuantity === 0;
+      const isOnGoing = (product.remainingQuantity ?? 0) < (product.quantity ?? 0);
 
-  get fromDate() {
-    if (!this.product?.startDate) {
-      return undefined;
-    }
+      if (isExpired) {
+        this.status.set(ProductStatus.Expired);
+        return;
+      }
 
-    return moment(this.product.startDate).format('DD/MM/YYYY');
+      if (isFutureProduct) {
+        this.fromDate.set(moment(startDate).format('DD/MM/YYYY'));
+        this.status.set(listingType === ListingType.SELL ? ProductStatus.Available : ProductStatus.Required);
+        return;
+      }
+
+      if (isSold) {
+        this.status.set(ProductStatus.Sold);
+        return;
+      }
+
+      if (isOnGoing) {
+        this.status.set(ProductStatus.Ongoing);
+        return;
+      }
+
+      this.status.set(listingType === ListingType.SELL ? ProductStatus.Available : ProductStatus.Required);
+    });
   }
 }
