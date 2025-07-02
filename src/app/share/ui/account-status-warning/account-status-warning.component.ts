460aa23d6f8f773a9e904e4754a1c08af0a7bbb0
duyta <duyta@b13technology.com>
2025-07-02 19:49:03 +0700

    CU-8699kb8cy: fix bug CTA button

--- src/app/share/ui/account-status-warning/account-status-warning.component.ts
+++ src/app/share/ui/account-status-warning/account-status-warning.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIcon } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { UserStatus } from 'app/models';
 import { AuthService } from 'app/services/auth.service';
@@ -8,14 +8,13 @@ import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-account-status-warning',
-  imports: [MatIconModule, RouterModule],
+  imports: [MatIcon, RouterModule],
   templateUrl: './account-status-warning.component.html',
   styleUrl: './account-status-warning.component.scss',
 })
 export class AccountStatusWarningComponent {
   authService = inject(AuthService);
   UserStatus = UserStatus;
-  // status = signal<UserStatus | undefined>(undefined);
   status = toSignal(
     this.authService.user$.pipe(
       filter((user) => !!user),
